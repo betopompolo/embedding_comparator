694bc1c8551965790f7ac544f19fb562702c8b73
Adalberto Pompolo <beto.nassu@gmail.com>
2023-05-04 21:12:00 -0300

    feat: validation summary

--- src/validation.py
+++ src/validation.py
@@ -1,6 +1,5 @@
-import itertools
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import List
 
 from tqdm import tqdm
 
@@ -26,48 +25,52 @@ class Validation:
 
   def run(self):
     queries_dataset = self.query_repository.get_dataset()
-    results: Dict[str, List[ValidationResult]] = {}
-    not_found_urls = []
+    results: List[ValidationResult] = []
+    not_found_count = 0
+    success_count = 0
+    error_count = 0
     
     for query in tqdm(queries_dataset, total=self.query_repository.get_dataset_count()):
       pair = self.cs_net_repository.search(query.url)
       if pair is None:
-        not_found_urls.append(query.url)
+        not_found_count += 1
         continue
-
+      
+      
       embedding_query = self.embedding_generator.from_text(
         self.pre_processer.process_text(query.query.split())
       )
       embedding_code = self.embedding_generator.from_code(self.pre_processer.process_code(pair.code_tokens))
       concatenated = self.embedding_concat.concatenate(embedding_code, embedding_query, reshape=(1, -1))
-      similarity = self.model.predict(concatenated, hide_logs=True)[:1]
+      prediction = self.model.predict(concatenated, hide_logs=True)
+      similarity = prediction[0][0]
       result = ValidationResult(
         query=query,
         code_url=pair.id,
         similarity=similarity,
       )
-
-      if query.query in results:
-        results[query.query].append(result)
+      results.append(result)
+    
+    for result in results:
+      if self.is_result_correct(result):
+        success_count += 1
       else:
-        results[query.query] = [result]
+        error_count += 1
+    print('Summary')
+    print(f'--------')
+    print(f'num_queries: {self.query_repository.get_dataset_count()}')
+    print(f'not found queries: {not_found_count}')
+    print(f'success: {success_count}')
+    print(f'error: {error_count}')
+    print(f'--------')
 
-    print('all queries were found!') if len(not_found_urls) == 0 else print(f'{len(not_found_urls)} queries were not found in code_search_net database')
-    
-    # TODO: Move!
-    # def print_query_results(search_term: str, results: List[ValidationResult]):
-    #   print(f'search_term: {search_term}')
-    #   for result in results:
-    #     print(f'code: {result.code_url} similarity: {result.similarity}')
-    #   print(f'----------------')
-    
-    # for search_term in results.keys():
-    #   results[search_term].sort(key=lambda result: result.similarity, reverse=True)
-    #   print_query_results(search_term, results[search_term])
-    #   break
+  def is_result_correct(self, result: ValidationResult):
+    ranges = [[0, 0.25], [0.25, 0.5], [0.5, 0.75], [0.75, 1]]
 
+    current_range = ranges[result.query.relevance]
 
-    
+    return current_range[0] <= result.query.relevance < current_range[1]
+
 Validation(
   cs_net_repository=CodeSearchNetDataset(),
   query_repository=QueryDataset(),
