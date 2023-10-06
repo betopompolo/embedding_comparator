4d1cdccdab13ec1086d65979f3cbfa9c4036cf77
Adalberto Pompolo <beto.nassu@gmail.com>
2023-10-06 15:50:16 -0300

    feat: triplet loss

--- src/models.py
+++ src/models.py
@@ -1,9 +1,9 @@
 from typing import Dict, List, Literal
 import tensorflow as tf
-from keras import Model, Sequential
-from keras.layers import Dense, Input, Concatenate, BatchNormalization, Dot, Add, LayerNormalization, Dropout, Conv1D, AveragePooling1D, Flatten
+from keras import Model, Sequential, backend as K
+from keras.layers import Dense, Input, Concatenate, BatchNormalization, Dot, Add, LayerNormalization, Dropout, Conv1D, AveragePooling1D, Flatten, Layer
 from keras.losses import BinaryCrossentropy, Loss, losses_utils, CosineSimilarity
-from keras.metrics import BinaryAccuracy, Precision, Recall
+from keras.metrics import BinaryAccuracy, Precision, Recall, Mean
 from keras.optimizers import Adam, RMSprop
 
 input_shape = (480)
@@ -224,3 +224,88 @@ def multilayer_raw(model_name: str, code_embedding_shape: tuple, comment_embeddi
   )
 
   return model
+
+
+class DistanceLayer(Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, anchor, positive, negative):
+        ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
+        an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
+        return (ap_distance, an_distance)
+    
+class EmbeddingComparator(Model):
+    def __init__(self, siamese_network, margin=0.5):
+      super().__init__()
+      self.siamese_network = siamese_network
+      self.margin = margin
+      self.loss_tracker = Mean(name="loss")
+
+    def call(self, inputs):
+        return self.siamese_network(inputs)
+
+    def train_step(self, data):
+      with tf.GradientTape() as tape:
+        loss = self._compute_loss(data)
+
+      gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+
+      self.optimizer.apply_gradients(
+        zip(gradients, self.siamese_network.trainable_weights)
+      )
+
+      self.loss_tracker.update_state(loss)
+      return {"loss": self.loss_tracker.result()}
+
+    def test_step(self, data):
+      loss = self._compute_loss(data)
+      self.loss_tracker.update_state(loss)
+      return {"loss": self.loss_tracker.result()}
+
+    def _compute_loss(self, data):
+      ap_distance, an_distance = self.siamese_network(data)
+
+      loss = ap_distance - an_distance
+      loss = tf.maximum(loss + self.margin, 0.0)
+      return loss
+
+    @property
+    def metrics(self):
+      return [self.loss_tracker]
+
+def triplet_accuracy(y_true, y_pred):
+  anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+  pos_dist = K.sum(K.square(anchor-positive),axis=1)
+  neg_dist = K.sum(K.square(anchor-negative),axis=1)
+  
+  return K.mean(pos_dist < neg_dist)
+    
+def embedding_comparator_model(input_shape, margin=0.5):
+  code_input = Input(
+    shape=input_shape,
+    name="code_embedding",
+  )
+  comment_positive_input = Input(
+    shape=input_shape,
+    name="comment_positive_embedding",
+  )
+  comment_negative_input = Input(
+    shape=input_shape,
+    name="comment_negative_embedding",
+  )
+
+  distance_layer = DistanceLayer()(
+    code_input,
+    comment_positive_input,
+    comment_negative_input,
+  )
+
+  model = Model(inputs=[code_input, comment_positive_input, comment_negative_input], outputs=distance_layer)
+  comp = EmbeddingComparator(model, margin=margin)
+  comp.compile(
+    optimizer=Adam(0.01),
+    metrics=[triplet_accuracy]
+  )
+
+  return comp
