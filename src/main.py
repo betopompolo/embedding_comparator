bcc94394dd02ee1b117c7689db0b8caf541bf863
Adalberto Pompolo <beto.nassu@gmail.com>
2024-02-08 10:37:54 -0300

    chore: remove unused files

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_from_disk
-
-train_pairs = load_from_disk('datasets/python_train/')
-
-def batch_mapping(batch):
-  return batch
-
-for x in train_pairs.map(batch_mapping, batch_size=100, batched=True):
-  pass
